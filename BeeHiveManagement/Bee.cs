3a36aaa7f01aa173f6cc87490700ac107ee97e97
Swdenis <swdenis@mail.ru>
2021-01-06 15:59:16 +0100

    * .gitignore: Initial commit
    * BeeHiveManagement.sln:
    * Bee.cs:
    * Queen.cs:
    * App.razor:
    * EggCare.cs:
    * Program.cs:
    * HoneyVault.cs:
    * _Imports.razor:
    * Index.razor:
    * index.html:
    * NectarCollector.cs:
    * Counter.razor:
    * app.css:
    * favicon.ico:
    * HoneyManufacturer.cs:
    * NavMenu.razor:
    * MainLayout.razor:
    * BeeHiveManagement.csproj:
    * launchSettings.json:
    * weather.json:
    * README.md:
    * ICON-LICENSE:
    * FONT-LICENSE:
    * bootstrap.min.css:
    * bootstrap.min.css.map:
    * open-iconic.eot:
    * open-iconic.otf:
    * open-iconic.svg:
    * open-iconic.ttf:
    * open-iconic.woff:
    * open-iconic-bootstrap.min.css:

---
+++ BeeHiveManagement/Bee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace BeeHiveManagement
+{
+    class Bee
+    {
+        public virtual float CostPerShift { get; }
+        public string Job { get; private set; }
+        public Bee(string job)
+        {
+            Job = job;
+        }
+        public void WorkTheNextShift()
+        {
+            if (HoneyVault.ConsumeHoney(CostPerShift))
+            {
+                DoJob();
+            }
+        }
+        protected virtual void DoJob() { /* the subclass overrides this */ }
+    }
+}
---
+++ BeeHiveManagement/HoneyVault.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace BeeHiveManagement
+{
+    static class HoneyVault
+    {
+        public const float NECTAR_CONVERSION_RATIO = .19f;
+        public const float LOW_LEVEL_WARNING = 10f;
+        private static float honey = 25f;
+        private static float nectar = 100f;
+        /* littile f or (float)? try casting differently and without*/
+
+        public static void CollectNectar(float amount)
+        {
+            if (amount > 0f) nectar += amount;
+        }
+
+        public static void ConvertNectarToHoney (float amount)
+        {
+            float nectarToConvert = amount;
+            if (nectarToConvert > nectar) nectarToConvert = nectar;
+            nectar -= nectarToConvert;
+            honey += nectarToConvert * NECTAR_CONVERSION_RATIO;
+        }
+
+        public static bool ConsumeHoney(float amount)
+        {
+            if (honey>= amount)
+            {
+                honey -= amount;
+                return true;
+            }
+
+            return false;
+        }
+
+        public static string StatusReport
+        {
+            get
+            {
+                string status = $"{honey:0.0} units of honey\n" +
+                    $"{nectar:0.0} units of nectar\n";
+                string warnings = "";
+                if (honey < LOW_LEVEL_WARNING) warnings += "\nLOW HONEY - ADD A HONEY MANUFACTURER";
+                if (nectar < LOW_LEVEL_WARNING) warnings += "\nLOW NECTAR - ADD A NECTAR COLLECTOR";
+                return status + warnings; 
+            }
+        }
+    }
+}
